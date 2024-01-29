4f1278170d6856e1d2bb04e1b1273b5672757504
vboxuser <alexandrdzhus@gmail.com>
2024-01-29 09:30:52 +0300

    add new path in config urls to new apps orders and users

--- luxfur/config/urls.py
+++ luxfur/config/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
+    path('users', include('users.urls', namespace='users')),
+    path('orders', include('orders.urls', namespace='orders')),
 ]
