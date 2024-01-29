a2c5e93ec0798e0f279adaf22555d30b14c45edc
vboxuser <alexandrdzhus@gmail.com>
2024-01-29 10:01:35 +0300

    add path to media and add AUTH_USER_MODEL

--- luxfur/config/settings.py
+++ luxfur/config/settings.py
@@ -123,7 +123,11 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+MEDIA_ROOT = BASE_DIR / 'media'
 
+MEDIA_URL = 'media/'
+
+AUTH_USER_MODEL = 'users.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
