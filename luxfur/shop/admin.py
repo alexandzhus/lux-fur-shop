5f3b330cb2dbda73335c9f4a2aec78576cf72229
vboxuser <alexandrdzhus@gmail.com>
2024-01-30 15:57:05 +0300

    create new class for admin-panel

--- luxfur/shop/admin.py
+++ luxfur/shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ['name', 'slug', 'image', 'description', 'material', 'category', 'height', 'length', 'width']
+    list_display = ['name', 'slug', 'image', 'material', 'time_create', 'time_update', 'category', 'height', 'length', 'width']
+    list_display_links = ['name', 'slug', 'image']
+
+
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", 'name')
+    list_display_links = ("id", 'name')
