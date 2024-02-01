c0dd659678435a1809b022263b58e715ab6e7320
vboxuser <alexandrdzhus@gmail.com>
2024-02-01 09:17:17 +0300

    create product for adminsite

--- luxfur/shop/admin.py
+++ luxfur/shop/admin.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from .models import *
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    fields = ['name', 'slug', 'image', 'description', 'material', 'category', 'height', 'length', 'width']
-    list_display = ['name', 'slug', 'image', 'material', 'time_create', 'time_update', 'category', 'height', 'length', 'width']
+    fields = ['name', 'slug', 'image', 'description', 'vendor_code',
+              'quantity', 'material', 'category', 'height', 'length', 'width']
+    list_display = ['name', 'slug', 'image', 'vendor_code',
+                    'quantity', 'material', 'time_create', 'time_update', 'category', 'height', 'length', 'width']
     list_display_links = ['name', 'slug', 'image']
 
 
