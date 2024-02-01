55a027ef64283baecb7d064284d8e5df065632f4
vboxuser <alexandrdzhus@gmail.com>
2024-02-01 09:09:53 +0300

    create Login class in view

--- luxfur/users/views.py
+++ luxfur/users/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .forms import *
+
+class Login(LoginView):
+    """
+    Клас для авторизации пользователя
+    """
+    authentication_form = LoginUserForm
+    template_name = 'users/login_form.html'
+    extra_context = {
+        'title': 'Авторизация'
+    }
+
+    def get_success_url(self):
+        if self.request.user.is_superuser:
+            return reverse_lazy('admin:index')
+
+        return reverse_lazy('home')
+
+
+
