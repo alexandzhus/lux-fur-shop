75bbe7f5742296b6073c6586a5e0c9276dff032d
vboxuser <alexandrdzhus@gmail.com>
2024-02-01 09:11:10 +0300

    create new path in users.urls for login and logout

--- luxfur/users/urls.py
+++ luxfur/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from .views import *
@@ -5,5 +6,6 @@ from .views import *
 app_name = 'users'
 
 urlpatterns = [
-
+    path('login/', Login.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
