397e77420ce0b977078703b1b1c92e5dfa78aca6
vboxuser <alexandrdzhus@gmail.com>
2024-02-01 09:12:51 +0300

    add new context_processors and login logout redirect

--- luxfur/config/settings.py
+++ luxfur/config/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'shop.context_processors.get_shop_context',
             ],
         },
     },
@@ -136,3 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'users.User'
+
+
+LOGIN_REDIRECT_URL = 'home'  # Явный путь при успешном входе на сайте
+LOGOUT_REDIRECT_URL = 'users:login'  # путь при успешном выходе с сайта
