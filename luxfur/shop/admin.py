44505c38439b7fcac4adc731a9aba3cbcde76f2f
vboxuser <alexandrdzhus@gmail.com>
2024-02-05 16:04:46 +0300

    поправил админку: добавил отображение изображений, также зарегистрировал модель материалы

--- luxfur/shop/admin.py
+++ luxfur/shop/admin.py
@@ -1,18 +1,54 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import *
+
+
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    fields = ['name', 'slug', 'image', 'description', 'vendor_code',
+    fields = ['name', 'slug', 'image', 'description', 'price', 'vendor_code',
               'quantity', 'material', 'category', 'height', 'length', 'width']
-    list_display = ['name', 'slug', 'image', 'vendor_code',
-                    'quantity', 'material', 'time_create', 'time_update', 'category', 'height', 'length', 'width']
-    list_display_links = ['name', 'slug', 'image']
+    list_display = ['name', 'slug', 'product_image', 'price', 'vendor_code',
+                    'quantity', 'material', 'time_create',
+                    'time_update', 'category', 'height', 'length', 'width', 'brief_descriptions']
+    list_display_links = ['name', 'slug']
+    prepopulated_fields = {'slug': ("name",)}
+    list_filter = ('name', 'price', 'quantity')
+    list_editable = ('price', 'quantity',)
+    list_per_page = 5
+    save_on_top = True
 
+    @admin.display(description="Изображение")
+    def product_image(self, product: Product):
+        """
+        Метод позволяет отображать поле с изображением в админке
+        :param product:
+        :return: str
+        """
+
+        if product.image:
+            return mark_safe(f'<img src="{product.image.url}" width=50>')
+        else:
+            return "без изображения"
+
+    @admin.display(description="Краткое описание товара")
+    def brief_descriptions(self, product: Product):
+
+        return f'{product.description[0:75]}' + "..."
 
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("id", 'name')
+    list_display = ("id", 'name', 'slug')
     list_display_links = ("id", 'name')
+    prepopulated_fields = {'slug': ("name", )}
+
+
+@admin.register(Material)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", 'name', 'slug')
+    list_display_links = ("id", 'name')
+    prepopulated_fields = {'slug': ("name", )}
