a16b58aad0bfbee7f0c9db3933c49a96637db134
vboxuser <alexandrdzhus@gmail.com>
2024-02-14 09:51:27 +0300

    Пути для приложения orders

--- luxfur/orders/urls.py
+++ luxfur/orders/urls.py
@@ -5,5 +5,6 @@ from .views import *
 app_name = 'orders'
 
 urlpatterns = [
-
+    path('order-create/', order_create, name='order_create'),
+    path('order-detail/<int:pk>/', OrderDetail.as_view(), name='order_detail'),
 ]
