1320d6032794fcae3722482ad685b1fc3b0011ed
vboxuser <alexandrdzhus@gmail.com>
2024-02-14 09:47:41 +0300

    Добавил форму создания заказа

---
+++ luxfur/orders/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import *
+
+class OrderCreateForm(forms.ModelForm):
+
+
+
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'address', 'postal_code', 'city']
+
+
