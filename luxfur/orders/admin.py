efb7d7fcee58cf0f6e2f66d1920dac9ee722a6ac
vboxuser <alexandrdzhus@gmail.com>
2024-02-14 09:44:54 +0300

    Создал классы для отображения заказов и их содержимого в админке

--- luxfur/orders/admin.py
+++ luxfur/orders/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class OrderItemsTabularInline(admin.TabularInline):
+    model = OrderItems
+    raw_id_fields = ['product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'first_name', 'last_name', 'email', 'address', 'postal_code',
+                    'city', 'time_create', 'time_update', 'paid']
+    list_display_links = ['id', 'user','first_name']
+    list_filter = ['paid', 'time_create', 'time_update']
+    inlines = [OrderItemsTabularInline, ]
+
+
+@admin.register(OrderItems)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order', 'product', 'product_amount', 'product_price', 'order_total_cost']
+    list_display_links = ['id', 'order']
+
+    @admin.display(description="Общая стоимость")
+    def order_total_cost(self, order: OrderItems):
+        return OrderItems.get_cost(order)
