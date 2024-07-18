531c2b5a2ae199d7b51aaa55b9f1d373b2d94fab
vboxuser <alexandrdzhus@gmail.com>
2024-07-18 09:39:59 +0300

    Убрал лишние пробелы.

--- luxfur/orders/forms.py
+++ luxfur/orders/forms.py
@@ -3,8 +3,6 @@ from .models import *
 
 class OrderCreateForm(forms.ModelForm):
 
-
-
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'phone_number', 'address', 'postal_code', 'city']
